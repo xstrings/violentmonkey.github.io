68ed12dee7673cb5dc7437a5539bac31887f4963
gera2ld <gera2ld@live.com>
2020-12-01 06:17:12 +0000

    Deploying to master from source @ 12f035f16ee8f0d8260d94cec3d483f97d3798c2 🚀

---
+++ component---src-pages-404-js-6880c73796ce5b7ba50e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var u=n("q1tI"),a=n.n(u);function o(){return a.a.createElement("main",null,a.a.createElement("h1",null,"Not Found"),a.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6880c73796ce5b7ba50e.js.map
